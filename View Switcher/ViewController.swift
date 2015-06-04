fbea5547717a903f4991cb032e700cd0f4f87f68
Andrew Watts <wattsap@appstate.edu>
2015-06-03 20:22:15 -0400

    Initial Commit

---
+++ View Switcher/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  View Switcher
+//
+//  Created by Andrew Watts on 6/3/15.
+//  Copyright (c) 2015 Andrew Watts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
